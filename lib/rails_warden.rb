7159abb36b64c2a6be14786a44d60418fcda03c3
Daniel Neighman <has.sox@gmail.com>
2009-12-07 22:59:49 +1100

    Updates rails warden to the new serializer type

--- lib/rails_warden.rb
+++ lib/rails_warden.rb
@@ -53,20 +53,19 @@ if defined?(Rails)
   end
 end
 
-# Session Serialization in.  This block determines how the user will
-# be stored in the session.  If you're using a complex object like an
-# ActiveRecord model, it is not a good idea to store the complete object.
-# An ID is sufficient
-Warden::Manager.serialize_into_session{ |user| [user.class, user.id] }
-
-# Session Serialization out.  This block gets the user out of the session.
-# It should be the reverse of serializing the object into the session
-Warden::Manager.serialize_from_session do |klass, id|
-  klass = case klass
-  when Class
-    klass
-  when String, Symbol
-    klass.to_s.classify.constantize
+class Warden::Serializers::Session
+  def serialize(user)
+    [user.class, user.id]
   end
-  klass.find(id)
+
+  def deserialize(key)
+    klass, id = key
+    klass = case klass
+            when Class
+              klass
+            when String, Symbol
+              klass.to_s.classify.constantize
+            end
+    klass.find(id)
+  end
 end
