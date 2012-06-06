df30299942a6a54ef299e851472571e04377b9d9
grosser <grosser.michael@gmail.com>
2012-06-06 09:57:53 -0700

    run tests on all other versions of rails to make sure they pass

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,18 @@
 $TESTING=true
 $:.push File.join(File.dirname(__FILE__), '..', 'lib')
 
-require 'rails'
+require 'rails/version'
+
+if Rails::VERSION::MAJOR > 2
+  require 'rails'
+else
+  module Rails
+    class FakeConfig
+      def after_initialize
+      end
+    end
+    @@configuration = FakeConfig.new
+  end
+  require 'initializer'
+end
 require 'rails_warden'
