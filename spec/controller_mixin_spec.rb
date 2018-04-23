21826be93ab14e11b3cd2174673e53898abe7058
Olle Jonsson <olle.jonsson@gmail.com>
2018-04-23 14:56:09 +0200

    RSpec: expect().to asserts

--- spec/controller_mixin_spec.rb
+++ spec/controller_mixin_spec.rb
@@ -30,50 +30,50 @@ describe "rails_warden controller mixin" do
   end
   
   it "should setup the spec" do
-    @controller.warden.should_not be_nil
+    expect(@controller.warden).not_to be_nil
   end
   
   it "should provide access to the warden instance" do
-    @controller.warden.should == @controller.env["warden"]
+    expect(@controller.warden).to eq(@controller.env["warden"])
   end
   
   it "should run authenticate on warden" do
-    @mock_warden.should_receive(:authenticate).and_return(true)
+    expect(@mock_warden).to receive(:authenticate).and_return(true)
     @controller.authenticate
   end
   
   it "should run authenticate! on warden" do
-    @mock_warden.should_receive(:authenticate!).and_return(true)
+    expect(@mock_warden).to receive(:authenticate!).and_return(true)
     @controller.authenticate!
   end
   
   it "should run authenticate? on warden" do
-    @mock_warden.should_receive(:authenticated?).and_return(true)
+    expect(@mock_warden).to receive(:authenticated?).and_return(true)
     @controller.authenticated?
   end
   
   it "should proxy logged_in? to authenticated" do
-    @mock_warden.should_receive(:authenticated?).and_return(true)
+    expect(@mock_warden).to receive(:authenticated?).and_return(true)
     @controller.logged_in?
   end
   
   it "should run user on warden" do
-    @mock_warden.should_receive(:user).and_return(true)
+    expect(@mock_warden).to receive(:user).and_return(true)
     @controller.user
   end
   
   it "should run current_user on warden" do
-    @mock_warden.should_receive(:user).and_return(true)
+    expect(@mock_warden).to receive(:user).and_return(true)
     @controller.current_user
   end
 
   it "should set the user on warden" do
-    @mock_warden.should_receive(:set_user).and_return(true)
+    expect(@mock_warden).to receive(:set_user).and_return(true)
     @controller.user = User.new
   end
   
   it "should proxy logout to warden" do
-    @mock_warden.should_receive(:logout).and_return(true)
+    expect(@mock_warden).to receive(:logout).and_return(true)
     @controller.logout
   end
 end
--- spec/rails_warden_spec.rb
+++ spec/rails_warden_spec.rb
@@ -19,25 +19,25 @@ describe "rails_warden" do
 
   it "RailsWarden::Manager.new should return an instance of Warden::Manager" do
     r = RailsWarden::Manager.new(@app, :failure_app => "foo_failure", :defaults => :password)
-    r.should be_an_instance_of(Warden::Manager)
+    expect(r).to be_an_instance_of(Warden::Manager)
   end
 
   it "should set the failure application to FooFailure" do
     r = RailsWarden::Manager.new(@app, :failure_app => "foo_failure", :defaults => :password)
-    r.config.failure_app.should == FooFailure
+    expect(r.config.failure_app).to eq(FooFailure)
   end
 
   it "should set the default user to FooUser if specified" do
     r = RailsWarden::Manager.new(@app,  :failure_app  => "foo_failure",
                                         :defaults     => :password,
                                         :default_user => "foo_user")
-    RailsWarden.default_user_class.should == FooUser
+    expect(RailsWarden.default_user_class).to eq(FooUser)
   end
 
   it "should set the default user to User if there is none specified" do
     r = RailsWarden::Manager.new(@app,  :failure_app  => "foo_failure",
                                         :defaults     => :password)
-    RailsWarden.default_user_class.should == User
+    expect(RailsWarden.default_user_class).to eq(User)
   end
 
   it "should set the failure action when specified" do
@@ -45,14 +45,14 @@ describe "rails_warden" do
                                         :defaults     => :password,
                                         :unauthenticated_action => :bad_login
                                         )
-    RailsWarden.unauthenticated_action.should == "bad_login"
+    expect(RailsWarden.unauthenticated_action).to eq("bad_login")
   end
 
   it "should set the failure action to unauthenticated when not specified" do
     r = RailsWarden::Manager.new(@app,  :failure_app  => "foo_failure",
                                         :defaults     => :password
                                         )
-    RailsWarden.unauthenticated_action.should == "unauthenticated"
+    expect(RailsWarden.unauthenticated_action).to eq("unauthenticated")
   end
 
   it "should not add a before_failure callback each time it is created" do
@@ -61,7 +61,7 @@ describe "rails_warden" do
     RailsWarden::Manager.new(@app,  :failure_app  => "foo_failure",
                                     :defaults     => :password)
 
-    Warden::Manager._before_failure.size.should == original_number_of_callbacks
+    expect(Warden::Manager._before_failure.size).to eq(original_number_of_callbacks)
   end
 
 end
